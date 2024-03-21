a0bc4b1812b0c97177eeb661ffc5f863d5a22a31
Spook <spook.devv@gmail.com>
2024-03-21 11:01:01 -0400

    Updated home page

--- app/page.js
+++ app/page.js
@@ -1,20 +1,49 @@
 import Link from "next/link";
+import classes from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Time to get started!
-      </h1>
-      <p>
-        <Link href="/meals">Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">Share Meals</Link>
-      </p>
-      <p>
-        <Link href="/community">Community</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideShow}></div>
+        <div>
+          <div className={classes.hero}>
+            <h1>Next Level Eatery for Next Level Foodies</h1>
+            <p>Taste & share food from all over the world</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            Next Level Eatery is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            Next Level Eatery is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={classes.section}>
+          <h2>Why Next Level Eatery?</h2>
+          <p>
+            Next Level Eatery is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            Next Level Eatery is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
