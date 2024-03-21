793816d13180c9075bae5d508a3d9b95d2ce8d79
Spook <spook.devv@gmail.com>
2024-03-20 21:15:13 -0400

    Updated header

---
+++ components/main-header/main-header.js
@@ -0,0 +1,30 @@
+import logoImg from "@/assets/logo.png";
+import Link from "next/link";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="a plate with food on it" priority />
+          Next Level Eatery
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodie Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default MainHeader;
