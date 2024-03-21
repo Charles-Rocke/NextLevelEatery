f1081752a4c3aed3c5297114c7daa0b46aa1d5df
Spook <spook.devv@gmail.com>
2024-03-21 11:14:07 -0400

    Server components vs Client Components (use client)

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideShow}></div>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
         <div>
           <div className={classes.hero}>
             <h1>Next Level Eatery for Next Level Foodies</h1>
