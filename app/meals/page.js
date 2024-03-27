bc72c009441c520e7f5fdb43367df5229a1a1fa4
Spook <spook.devv@gmail.com>
2024-03-27 13:29:50 -0400

    Fetching data from database and load them into the app

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/backend/lib/meals";
 
-function MealsPage() {
+async function MealsPage() {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +21,7 @@ function MealsPage() {
         </p>
       </header>
       <main className={classes.name}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
