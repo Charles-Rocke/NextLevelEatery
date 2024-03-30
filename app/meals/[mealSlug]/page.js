7aa51e03167289ad5f33dc5fc079839bc5f5866e
Spook <spook.devv@gmail.com>
2024-03-30 12:41:38 -0400

    Added dynamic metadata

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -2,6 +2,19 @@ import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/backend/lib/meals";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
